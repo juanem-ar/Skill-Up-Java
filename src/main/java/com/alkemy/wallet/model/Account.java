86392ba19e674f0b23322b9d3a46c4a7065f446c
Francortiz-137 <francortiz.c@gmail.com>
2022-11-01 15:04:53 -0300

    #4hem4mw: Fixed foreign key to many-to-one

--- src/main/java/com/alkemy/wallet/model/Account.java
+++ src/main/java/com/alkemy/wallet/model/Account.java
@@ -39,7 +39,7 @@ public class Account {
     @NotNull
     private Double balance;
 
-    @OneToOne(cascade = CascadeType.ALL)
+    @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "account_id", referencedColumnName = "id")
     private long userId;
 

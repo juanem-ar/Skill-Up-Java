03c68d11b87031fce3672f042851228f706995f0
Francortiz-137 <francortiz.c@gmail.com>
2022-11-02 15:29:26 -0300

    Modified: UserService, UserServiceImpl to create findById, fixed AccountRepository name to IAccountRepository according to standards

--- src/main/java/com/alkemy/wallet/service/IUserService.java
+++ src/main/java/com/alkemy/wallet/service/IUserService.java
@@ -1,5 +1,11 @@
 package com.alkemy.wallet.service;
 
+import com.alkemy.wallet.model.User;
+
+import java.util.Optional;
+
 public interface IUserService {
     public String deleteUser(Long id);
+
+    Optional<User> findById(Long id);
 }
--- src/main/java/com/alkemy/wallet/service/impl/UserServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/impl/UserServiceImpl.java
@@ -1,8 +1,11 @@
 package com.alkemy.wallet.service.impl;
 
+import com.alkemy.wallet.model.User;
 import com.alkemy.wallet.repository.IUserRepository;
 import com.alkemy.wallet.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
 
 public class UserServiceImpl implements IUserService {
     private IUserRepository  iUserRepository;
@@ -16,5 +19,10 @@ public class UserServiceImpl implements IUserService {
     public String deleteUser(Long id) {
         iUserRepository.deleteById(id);
         return "delete user with number" + id ;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return iUserRepository.findById(id);
     }
 }

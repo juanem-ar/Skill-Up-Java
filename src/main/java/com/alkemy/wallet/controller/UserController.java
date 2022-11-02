5d0ecd23a7ad110bf6aa0fe10c0934a0be5f4034
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-11-02 19:23:42 -0300

    feat(controller): secured endpoint users

--- src/main/java/com/alkemy/wallet/controller/UserController.java
+++ src/main/java/com/alkemy/wallet/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ public class UserController {
         return new ResponseEntity<>(userService.deleteUser(id), HttpStatus.OK);
     }
     
+    @Secured(value = { "ROLE_ADMIN" })
     @GetMapping
     public ResponseEntity<List<ResponseUserDto>> findAllUsers () {
     	return ResponseEntity.ok(userService.findAllUsers());

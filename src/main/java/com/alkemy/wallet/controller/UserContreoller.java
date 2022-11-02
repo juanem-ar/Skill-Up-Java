2323ee175a17821f757267b94e17ad5832224910
santiago <santysjr956@gmail.com>
2022-11-01 22:31:11 -0300

    structure folder service and controller

---
+++ src/main/java/com/alkemy/wallet/controller/UserContreoller.java
@@ -0,0 +1,15 @@
+package com.alkemy.wallet.controller;
+
+import com.alkemy.wallet.service.impl.UserServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("api/v1/users")
+@RestController
+public class UserContreoller {
+
+}
---
+++ src/main/java/com/alkemy/wallet/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.alkemy.wallet.service.impl;
+
+import com.alkemy.wallet.service.IUserService;
+
+public class UserServiceImpl implements IUserService {
+
+    
+}

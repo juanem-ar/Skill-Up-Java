f274be9db75627e1067f0be5090a25cff137c1fc
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-11-02 12:43:45 -0300

    feat(service): add method to get all users

--- src/main/java/com/alkemy/wallet/service/IUserService.java
+++ src/main/java/com/alkemy/wallet/service/IUserService.java
@@ -1,5 +1,11 @@
 package com.alkemy.wallet.service;
 
+import java.util.List;
+
+import com.alkemy.wallet.dto.ResponseUserDto;
+
 public interface IUserService {
     public String deleteUser(Long id);
+    
+    public List<ResponseUserDto> findAllUsers();
 }

142dc0cc85aaccda51aa5332b60126dfd25c883d
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-11-02 10:13:03 -0300

    feat(model): add relation OneToMany from user to Account

--- src/main/java/com/alkemy/wallet/model/User.java
+++ src/main/java/com/alkemy/wallet/model/User.java
@@ -1,7 +1,9 @@
 package com.alkemy.wallet.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -48,7 +51,9 @@ public class User {
 	@ManyToOne
 	@JoinColumn(name = "role_id")
 	private Role role;
-	// account
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<Account> accounts;
 	
 	@CreationTimestamp
 	private Timestamp creationDate;

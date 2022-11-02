c76a78c30d4446c1d5cef9f8506cdbbaeb908f49
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-11-02 12:46:13 -0300

    feat(controller): add endpoint to get all users

--- src/main/java/com/alkemy/wallet/controller/UserController.java
+++ src/main/java/com/alkemy/wallet/controller/UserController.java
@@ -1,20 +1,32 @@
 package com.alkemy.wallet.controller;
 
-import com.alkemy.wallet.service.impl.UserServiceImpl;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alkemy.wallet.dto.ResponseUserDto;
+import com.alkemy.wallet.service.impl.UserServiceImpl;
+
 @RequestMapping("api/v1/users")
 @RestController
 public class UserController {
+	@Autowired
     private UserServiceImpl userService;
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUsers(@PathVariable Long id){
         return new ResponseEntity<>(userService.deleteUser(id), HttpStatus.OK);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<ResponseUserDto>> findAllUsers () {
+    	return ResponseEntity.ok(userService.findAllUsers());
     }
 }

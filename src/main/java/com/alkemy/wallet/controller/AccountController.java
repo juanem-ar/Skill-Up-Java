22d18cd24b9ee8d94fa9d9d8d3b4add940d80962
Francortiz-137 <francortiz.c@gmail.com>
2022-11-03 14:23:12 -0300

    minor fixes

--- src/main/java/com/alkemy/wallet/controller/AccountController.java
+++ src/main/java/com/alkemy/wallet/controller/AccountController.java
@@ -1,5 +1,6 @@
 package com.alkemy.wallet.controller;
 
+import com.alkemy.wallet.dto.ResponseAccountDto;
 import com.alkemy.wallet.mapper.IAccountMapper;
 import com.alkemy.wallet.model.User;
 import com.alkemy.wallet.service.IAccountService;
@@ -7,7 +8,10 @@ import com.alkemy.wallet.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.Optional;
 
 @RequestMapping("api/v1/accounts")
@@ -20,13 +24,12 @@ public class AccountController {
     @Autowired
     private IAccountMapper iAccountMapper;
 
+    @Secured(value = { "ROLE_ADMIN" })
     @GetMapping("{id}")
-    public ResponseEntity<Object> listAccountsByUser(@PathVariable Long id){
+    public ResponseEntity<List<ResponseAccountDto>> listAccountsByUser(@PathVariable Long id){
         Optional<User> user = userService.findById(id);
-
-        if(user.isEmpty())
-            return new ResponseEntity<>( "User Not Found", HttpStatus.NOT_FOUND);
-
+    //    if(user.isEmpty())
+    //        return new ResponseEntity<>( "User Not Found", HttpStatus.NOT_FOUND);
         return new ResponseEntity<>(iAccountMapper.accountsToAccountsDto(accountService.findAllByUser(user.get())), HttpStatus.OK);
     }
 }

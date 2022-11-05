9fe0f83f413bf12b0a4a27f15451d8da112de27a
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-11-04 21:13:11 -0300

    feat(service): add method to get balance of accounts

--- src/main/java/com/alkemy/wallet/service/impl/AccountServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/impl/AccountServiceImpl.java
@@ -1,18 +1,39 @@
 package com.alkemy.wallet.service.impl;
 
 
-import com.alkemy.wallet.model.Account;
-import com.alkemy.wallet.model.User;
-import com.alkemy.wallet.repository.IAccountRepository;
-import com.alkemy.wallet.service.IAccountService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.alkemy.wallet.dto.AccountBalanceDto;
+import com.alkemy.wallet.dto.ResponseUserBalanceDto;
+import com.alkemy.wallet.mapper.IAccountMapper;
+import com.alkemy.wallet.model.Account;
+import com.alkemy.wallet.model.EType;
+import com.alkemy.wallet.model.Transaction;
+import com.alkemy.wallet.model.User;
+import com.alkemy.wallet.repository.IAccountRepository;
+import com.alkemy.wallet.security.service.JwtUtils;
+import com.alkemy.wallet.service.IAccountService;
+import com.alkemy.wallet.service.ITransactionService;
+import com.alkemy.wallet.service.IUserService;
 @Service
 public class AccountServiceImpl implements IAccountService {
     private IAccountRepository iAccountRepository;
+    
+    @Autowired
+    private IUserService userService;
 
+    @Autowired
+    private JwtUtils jwtUtils;
+    
+    @Autowired
+    private ITransactionService transactionService;
+    
+    @Autowired
+    private IAccountMapper accountMapper;
+    
     @Autowired
     public AccountServiceImpl(IAccountRepository iAccountRepository) {
         this.iAccountRepository = iAccountRepository;
@@ -22,4 +43,43 @@ public class AccountServiceImpl implements IAccountService {
     public List<Account> findAllByUser(User user) {
         return user.getAccounts();
     }
+
+	@Override
+	public ResponseUserBalanceDto getBalance(String token) {
+		Long userId = jwtUtils.extractUserId(token);
+		
+		User user = userService.getUserById(userId);
+		
+		ResponseUserBalanceDto responseUserBalanceDto = new ResponseUserBalanceDto();
+		responseUserBalanceDto.setId(userId);
+		
+		for(Account account : user.getAccounts()) {
+			AccountBalanceDto accountBalanceDto = accountMapper.accountToBalanceDto(account);
+			
+			accountBalanceDto.setBalance(
+				calcularBalance(
+					account.getBalance(),
+					transactionService.findAllTransactionsWith(account)));
+
+			responseUserBalanceDto.getAccountBalanceDtos().add(accountBalanceDto);
+		}
+		
+		return responseUserBalanceDto;
+	}
+
+	
+	private Double calcularBalance(
+		Double balanceBase, List<Transaction> transactions) {
+		Double b = balanceBase;
+		
+		for(Transaction transaction : transactions) {
+			if(transaction.getType() == EType.DEPOSIT
+				|| transaction.getType() == EType.INCOME)
+				b += transaction.getAmount();
+			else if(transaction.getType() == EType.PAYMENT)
+				b -= transaction.getAmount();
+		}
+		
+		return b;
+	}
 }
---
+++ src/test/java/com/alkemy/wallet/service/impl/AccountServiceImplTest.java
@@ -0,0 +1,108 @@
+package com.alkemy.wallet.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.alkemy.wallet.dto.ResponseUserBalanceDto;
+import com.alkemy.wallet.mapper.IAccountMapper;
+import com.alkemy.wallet.model.Account;
+import com.alkemy.wallet.model.EType;
+import com.alkemy.wallet.model.Transaction;
+import com.alkemy.wallet.model.User;
+import com.alkemy.wallet.repository.IAccountRepository;
+import com.alkemy.wallet.security.service.JwtUtils;
+import com.alkemy.wallet.service.ITransactionService;
+import com.alkemy.wallet.service.IUserService;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+class AccountServiceImplTest {
+	@Mock
+	private IAccountRepository AccountRepository;
+
+	@Mock
+	private IUserService userService;
+
+	@Mock
+	private JwtUtils jwtUtils;
+
+	@Mock
+	private ITransactionService transactionService;
+
+	@Autowired
+	@Spy
+	private IAccountMapper accountMapper;
+
+	@InjectMocks
+	private AccountServiceImpl accountService;
+
+
+	@Test
+	void getBalance_AccountWithThreeTransactions_ExactBalance() {
+		String token = "token";
+		Long userId = 1L;
+		when(jwtUtils.extractUserId(token)).thenReturn(userId);
+
+		// three transactions
+		List<Transaction> transactions = new ArrayList<>();
+
+		Double incomeAmount = 1000.0;
+		Transaction income = new Transaction();
+		income.setType(EType.INCOME);
+		income.setAmount(incomeAmount);
+		transactions.add(income);
+
+		Double paymentAmount = 100.0;
+		Transaction payment = new Transaction();
+		payment.setType(EType.PAYMENT);
+		payment.setAmount(paymentAmount);
+		transactions.add(payment);
+
+		Double depositAmount = 10.0;
+		Transaction deposit = new Transaction();
+		deposit.setType(EType.DEPOSIT);
+		deposit.setAmount(depositAmount);
+		transactions.add(deposit);
+		//
+
+		Double balanceBase = 1.0;
+		Account account = new Account();
+		account.setBalance(balanceBase);
+
+		User user = new User();
+		user.setAccounts(List.of(account));
+
+		when(userService.getUserById(userId)).thenReturn(user);
+
+		when(transactionService.findAllTransactionsWith(account))
+			.thenReturn(transactions);
+
+		Double balanceFinal =
+			incomeAmount + depositAmount + balanceBase
+				- paymentAmount;
+
+		ResponseUserBalanceDto result =
+			accountService.getBalance(token);
+
+		assertEquals(1, result.getAccountBalanceDtos().size());
+		
+		double epsilon = 0.000001d;
+		assertEquals(
+			balanceFinal,
+			result.getAccountBalanceDtos().get(0).getBalance(),
+			epsilon);
+	}
+
+}

ab34203a68e14855518c32b04b616554cbf264c9
santiago <santysjr956@gmail.com>
2022-11-04 23:16:45 -0300

    fix code

--- src/main/java/com/alkemy/wallet/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/impl/TransactionServiceImpl.java
@@ -8,27 +8,24 @@ import com.alkemy.wallet.model.EType;
 import com.alkemy.wallet.model.Transaction;
 import com.alkemy.wallet.repository.ITransactionRepository;
 import com.alkemy.wallet.service.ITransactionService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
+@RequiredArgsConstructor
 public class TransactionServiceImpl implements ITransactionService {
 
-    private ITransactionRepository iTransactionRepository;
-    private ITransactionMapper iTransactionMapper;
+    private final ITransactionRepository iTransactionRepository;
+    private final ITransactionMapper iTransactionMapper;
 
-    @Autowired
-    public TransactionServiceImpl(ITransactionRepository iTransactionRepository, ITransactionMapper iTransactionMapper) {
-        this.iTransactionRepository = iTransactionRepository;
-        this.iTransactionMapper = iTransactionMapper;
-    }
 
     @Override
     public TransactionDtoPay payment( TransactionDtoPay transitionDtoPay) {
        Transaction transaction = iTransactionMapper.transactionDtoToTransaction(transitionDtoPay);
         transaction.setType(EType.PAYMENT);
-        //transaction.setAccountId(1L);
         iTransactionRepository.save(transaction);
         TransactionDtoPay  transactionDtoPay = iTransactionMapper.transactionToTransactionDto(transaction);
         return transactionDtoPay;

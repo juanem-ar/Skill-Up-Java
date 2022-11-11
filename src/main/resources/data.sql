c9c041c5870d19b6c4e47e66b9d0ba25a919600b
Adrian Leguizamon <adrian.luis.leguizamon@gmail.com>
2022-11-11 10:11:29 -0300

    fix: crash while populate table roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,10 @@
-INSERT INTO roles(id, name) VALUES ('1', 'ADMIN');
-INSERT INTO roles(id, name) VALUES ('2', 'USER');
+insert into wallet.roles(id, name)
+	select 1, 'ADMIN'
+where not exists
+	(select * from roles where id = 1 and name = 'ADMIN');
+
+	
+insert into roles(id, name)
+		select 2, 'USER'
+where not exists
+	(select * from roles where id = 2 and name = 'USER');

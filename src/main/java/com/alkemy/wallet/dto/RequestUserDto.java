5c4fc5faa22b9cf23df89a4e2320013ef78dda6c
Juan López <juanem@hotmail.com>
2022-11-30 11:35:15 -0300

    added Schema definitions for RequestUserDto

--- src/main/java/com/alkemy/wallet/dto/RequestUserDto.java
+++ src/main/java/com/alkemy/wallet/dto/RequestUserDto.java
@@ -1,6 +1,7 @@
 package com.alkemy.wallet.dto;
 
 import com.alkemy.wallet.model.Role;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,16 +17,25 @@ public class RequestUserDto {
 
     @Size(min = 3, max = 50)
     @NotNull
+    @Schema(required = true, example = "Jhon", description = "User name", minLength = 3, maxLength = 50)
     private String firstName;
+
     @NotNull
     @Size(min = 3, max = 50)
+    @Schema(required = true, example = "McKein", description = "User lastname", minLength = 3, maxLength = 50)
     private String lastName;
+
     @NotNull
     @Email
     @Size(min = 6)
+    @Schema(required = true, pattern = "[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,}", example = "jhonMcKein@hotmail.com", minLength = 6, description = "User email")
     private String email;
+
     @NotNull
-    @Length(min = 8)
+    @Length(min = 8, max = 25)
+    @Schema(required = true, example = "12345678", description = "Password", minLength = 8, maxLength = 25)
     private String password;
+
+    @Schema(required = true)
     private Role role;
 }
